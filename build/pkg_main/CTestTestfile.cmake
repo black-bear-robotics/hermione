a658fcf2a5ae3a6c1ab6c64d45fe5d1f79add6bb
mattvh <matt@ntugo.com>
2019-03-15 11:07:22 -0400

    Removing ROS/catkin stuff from repository.

--- build/pkg_main/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nvidia/hermione_ws/src/pkg_main
-# Build directory: /home/nvidia/hermione_ws/build/pkg_main
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
